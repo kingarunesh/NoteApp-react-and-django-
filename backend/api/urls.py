ecc17a06f5b2107602b2c19a9528fed4417da9c1
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-12-07 06:39:34 +0530

    update method added in backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,6 @@ from api.views import *
 urlpatterns = [
     path("", getRoutes, name="routes"),
     path("notes/", getNotes, name="notes"),
-    path("note/<str:pk>/", getNote, name="note")
+    path("note/<str:pk>/", getNote, name="note"),
+    path("note/<str:pk>/update/", update_note, name="update-note"),
 ]
